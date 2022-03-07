992cd6f71b263e54db3208396273bb8353996796
jinkim0517 <90814113+jinkim0517@users.noreply.github.com>
2022-03-06 23:19:41 -0800

    assignmetn 3 done

--- Assignments/a3.cpp
+++ Assignments/a3.cpp
@@ -76,21 +76,17 @@ class Bulleted_list : public List_base  {
     return bullet;
   }
 
-  void reverse_order() { // fix
-    vector<string> temp_lst;
-    for (int i = lst.size(); i >= 0; i--) {
-      temp_lst.push_back(lst[i]);
-    }
-    lst = temp_lst;
+  void reverse_order() {
+    reverse(lst.begin(), lst.end());
   }
 
   string to_html_str() const {
     string result;
     result += "<ul>\n";
     for (int i = 0; i < lst.size(); i++) {
-      result += " <li>" + lst[i] + "<li>\n";
+      result += "  <li>" + lst[i] + "<li>\n";
     }
-    result += "</ul>";
+    result += "</ul>\n";
     return result;
   }
 
@@ -98,190 +94,184 @@ class Bulleted_list : public List_base  {
   {  }
 };
 
-void demo1() {
-    Bulleted_list lst;
+class Numbered_list : public List_base {
+private:
+  int start;
+  bool reversed;
+  string num_sep;
+  vector<string> lst;
+public:
+  Numbered_list() 
+  : start(1), reversed(false), num_sep(". "), lst()
+  { }
 
-    lst.add_to_end("oranges");
-    lst.add_to_end("pears");
-    lst.add_to_end("grapes");
-    lst.add_to_start("apples");
+  string to_str() const{
+    vector<string> numbers;
+    string s_result;
+    
+    for (int i = start; i < start + lst.size(); i++) {
+      numbers.push_back(to_string(i));
+    }
 
-    cout << "\n";
-    cout << lst.to_str();
+    if (reversed) {
+      reverse(numbers.begin(), numbers.end());
+    }
+    
+    for (int i = 0; i < lst.size(); i++) {
+      s_result += get_indent() + numbers[i] + num_sep + lst[i] + '\n';
+    }
 
-    lst.set_indent(3);
-    cout << "\n";
-    cout << lst.to_str();
+    return s_result;
+  }
 
-    lst.set_indent(0);
-    lst.set_bullet("** ");
-    cout << "\n";
-    cout << lst.to_str();
+  void add_to_end(const string& s) {
+    lst.push_back(s);
+  } 
 
-    lst.set_bullet("- ");
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_str();
+  void add_to_start(const string& s) {
+    lst.insert(lst.begin(), s);
+  }
 
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_html_str();
+  void set_num_sep(string s) {
+    num_sep = s;
+  }
+
+  string get_num_sep() const {
+    return num_sep;
+  }
+
+  string get(int i) const{
+    return lst[i];
+  }
+
+  int get_start() const{
+    return start;
+  }
+
+  int size() const {
+    return lst.size();
+  }
+
+  void set_start_num(int s) {
+    if (s >= 0) {
+      start = s;
+    }
+    else {
+      start = 0;
+    }
+  }
+
+  void reverse_order() {
+    reverse(lst.begin(), lst.end());
+    if (reversed) {
+      reversed = false;
+    }
+    else {
+      reversed = true;
+    }
+  }
+
+  string to_html_str() const {
+    string result;
+    if (reversed) {
+      result += "<ol reversed>\n";
+    }
+    else {
+      result += "<ol>\n";
+    }
+    for (int i = 0; i < lst.size(); i++) {
+      result += "  <li>" + lst[i] + "<li>\n";
+    }
+    result += "</ol>\n";
+    return result;
+  }
+
+  ~Numbered_list() 
+  { }
+};
+
+void bullet_lst_test() {
+  Bulleted_list test1;
+  Bulleted_list test3;
+  Bulleted_list test4;
+
+  test1.add_to_end("hey");
+  test1.add_to_end("hi");
+  test1.add_to_end("hello");
+  test1.add_to_end("wassup");
+  test1.add_to_start("yo!");
+
+  assert(test1.to_str() == " - yo!\n - hey\n - hi\n - hello\n - wassup\n");
+  cout << "Bulleted List Test 1 Passed...\n";
+
+  test1.reverse_order();
+
+  assert(test1.to_str() == " - wassup\n - hello\n - hi\n - hey\n - yo!\n");
+  cout << "Bulleted List Test 2 Passed...\n";
+
+  for (int i = 0; i < 5; i++) {
+    test3.add_to_end("");
+  }
+
+  assert(test3.to_str() == " - \n - \n - \n - \n - \n");
+  cout << "Bulleted List Test 3 Passed...\n";
+
+  test4.add_to_end("apple");
+  test4.add_to_end("orange");
+
+  assert(test4.to_html_str() == "<ul>\n  <li>apple<li>\n  <li>orange<li>\n</ul>\n");
+  cout << "Bulleted List Test 4 Passed...\n";
+
+  test4.reverse_order();
+
+  assert(test4.to_html_str() == "<ul>\n  <li>orange<li>\n  <li>apple<li>\n</ul>\n");
+  cout << "Bulleted List Test 5 Passed!\n";
+
+  cout << "--------------------------------\n";
+}
+
+void num_lst_test() {
+  Numbered_list test1;
+  Numbered_list test3;
+  Numbered_list test4;
+
+  test1.set_start_num(0);
+  test1.add_to_end("hey");
+  test1.add_to_end("hi");
+  test1.add_to_end("hello");
+  test1.add_to_end("wassup");
+  test1.add_to_start("yo!");
+
+  assert(test1.to_str() == "0. yo!\n1. hey\n2. hi\n3. hello\n4. wassup\n");
+  cout << "Numbered List Test 1 Passed...\n";
+
+  test1.reverse_order();
+
+  assert(test1.to_str() == "4. wassup\n3. hello\n2. hi\n1. hey\n0. yo!\n");
+  cout << "Numbered List Test 2 Passed...\n";
+
+  for (int i = 0; i < 5; i++) {
+    test3.add_to_end("");
+  }
+
+  assert(test3.to_str() == "1. \n2. \n3. \n4. \n5. \n");
+  cout << "Numbered List Test 3 Passed...\n";
+
+  test4.add_to_end("apple");
+  test4.add_to_end("orange");
+
+  assert(test4.to_html_str() == "<ol>\n  <li>apple<li>\n  <li>orange<li>\n</ol>\n");
+  cout << "Numbered List Test 4 Passed...\n";
+
+  test4.reverse_order();
+
+  assert(test4.to_html_str() == "<ol reversed>\n  <li>orange<li>\n  <li>apple<li>\n</ol>\n");
+  cout << "Numbered List Test 5 Passed!\n";
+
+  cout << "--------------------------------\n";
 }
 
 int main() {
-  // Bulleted_list l;
-
-  // l.add_to_end("hi");
-  // l.add_to_end("hey!");
-  // l.add_to_start("yo");
-
-  // l.set_indent(5);
-  // l.set_bullet("**********");
-  // cout << l.to_str();
-
-  demo1();
+  bullet_lst_test();
+  num_lst_test();
 }
-
-
-/*
-
-Below is the code used in the assignment description, along with its output.
-
-void demo1() {
-    Bulleted_list lst;
-
-    lst.add_to_end("oranges");
-    lst.add_to_end("pears");
-    lst.add_to_end("grapes");
-    lst.add_to_start("apples");
-
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_indent(3);
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_indent(0);
-    lst.set_bullet("** ");
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_bullet("- ");
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_html_str();
-}
-
-Output for demo1():
-
-- apples
-- oranges
-- pears
-- grapes
-
-   - apples
-   - oranges
-   - pears
-   - grapes
-
-** apples
-** oranges
-** pears
-** grapes
-
-- grapes
-- pears
-- oranges
-- apples
-
-<ul>
-  <li>apples</li>
-  <li>oranges</li>
-  <li>pears</li>
-  <li>grapes</li>
-</ul>
-
-
-void demo2() {
-    Numbered_list lst;
-
-    lst.add_to_end("oranges");
-    lst.add_to_end("pears");
-    lst.add_to_end("grapes");
-    lst.add_to_start("apples");
-
-    cout << lst.to_str();
-
-    lst.set_num_sep(") ");
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_start_num(3);
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_start_num(1);
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.set_start_num(3);
-    cout << "\n";
-    cout << lst.to_str();
-
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_html_str();
-
-    lst.reverse_order();
-    cout << "\n";
-    cout << lst.to_html_str();
-}
-
-Output for demo2():
-
-1. apples
-2. oranges
-3. pears
-4. grapes
-
-1) apples
-2) oranges
-3) pears
-4) grapes
-
-3) apples
-4) oranges
-5) pears
-6) grapes
-
-4) grapes
-3) pears
-2) oranges
-1) apples
-
-6) grapes
-5) pears
-4) oranges
-3) apples
-
-<ol>
-  <li>apples</li>
-  <li>oranges</li>
-  <li>pears</li>
-  <li>grapes</li>
-</ol>
-
-<ol reversed>
-  <li>grapes</li>
-  <li>pears</li>
-  <li>oranges</li>
-  <li>apples</li>
-</ol>
-
-*/
